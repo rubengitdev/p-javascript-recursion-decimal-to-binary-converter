a5886d2f9036f8df37610f57ebf99a882cbdd93f
rubengitdev <rubencahyadi504@gmail.com>
2026-07-21 21:45:08 +0700

    Added checkUserInput function

---
+++ script.js
@@ -0,0 +1,24 @@
+// HTML DOM Initialization
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+function checkUserInput() {
+    if (
+        !numberInput.value ||
+        isNaN(parseInt(numberInput.value)) ||
+        parseInt(numberInput.value) < 0
+    ) {
+        alert(`Please provide a decimal number greater than or equal to 0`);
+        return;
+    }
+    console.log(numberInput.value);
+}
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkUserInput();
+    }
+});
